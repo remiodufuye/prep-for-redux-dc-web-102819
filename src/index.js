29bc52dcf60bedbc064b0dd7ef9565e5bb7363ed
github username <github email address>
2020-01-25 14:30:23 -0500

    changes made

--- src/index.js
+++ src/index.js
@@ -1,25 +1,66 @@
+
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux'
+
+const initialState = {
+  count: 55,
+  user: "Chine"
+}
+
+const reducer = (oldState=initialState, action) => {
+  console.log("oldState:", oldState, "action:", action)
+  switch (action.type) {
+    case "INCREMENT_COUNT":
+      return {
+        ...oldState,
+        count: oldState.count + action.payload
+      } //newState
+    case "DECREMENT_COUNT":
+      return {
+        ...oldState,
+        count: oldState.count - action.payload
+      }
+    default:
+      return oldState //this is {count: 100}
+  }
+}
+//reducer is automatically invoked once at the start of your app
+//reducer is also invoked everytime we call store.dispatch
+
+const store = createStore(reducer)
+//.getState() -> read
+//.dispatch() -> update
 
 class App extends Component {
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Counter />
+        <Header/>
+        <Counter/>
       </div>
     );
   }
 }
 
 class Header extends Component {
+  increment = (num) => {
+    store.dispatch({type: "INCREMENT_COUNT", payload: num})
+  };
+
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={()=>this.increment(1)}> + </button>
+        <button onClick={()=>this.increment(3)}> +3 </button>
         <h1 className="App-title">Welcome to React</h1>
       </header>
     );
@@ -27,29 +68,19 @@ class Header extends Component {
 }
 
 class Counter extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
-  };
-
-  decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
-  };
-
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
+  decrement = (num) => {
+    store.dispatch({type: "DECREMENT_COUNT", payload: num})
   };
 
   render() {
+    let {count} = store.getState()
+
     return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+      <button onClick={()=>this.decrement(5)}> -5 </button>
+      <button onClick={()=>this.decrement(1)}> - </button>
+        <h1>{store.getState().count}</h1>
+        <h3>{`The current count is less than ${count + 5 - count % 5}`}</h3>
       </div>
     );
   }
